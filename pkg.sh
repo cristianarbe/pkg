69cf5997182ac35fbc99a39559ac8ceaa1cc37a6
Cristian Ariza <dev@cristianrz.com>
2020-02-09 16:56:32 +0000

    Change function names

--- pkg.sh
+++ pkg.sh
@@ -2,57 +2,63 @@
 set -e -u
 
 show_help() {
-	echo 'Usage: pkg command [arguments]'
-	echo ''
-	echo 'A tool for managing packages. Commands:'
-	echo ''
-	echo '  files <packages>'
-	echo '  install <packages>'
-	echo '  list-all'
-	echo '  list-installed'
-	echo '  reinstall <packages>'
-	echo '  search <query>'
-	echo '  show <packages>'
-	echo '  uninstall <packages>'
-	echo '  upgrade'
+	cat << 'EOF'
+Usage: pkg command [arguments]
+ 
+A tool for managing packages. Commands:
+ 
+  files <packages>
+  install <packages>
+  list-all
+  list-installed
+  reinstall <packages>
+  search <query>
+  show <packages>
+  uninstall <packages>
+  upgrade
+EOF
 	exit 1
 }
 
-check_pkgcache() {
-	if [ -z "$(find /var/cache/apt/pkgcache.bin -mmin -5)" ]; then
+pkg_update() {
+	if test -z "$(find /var/cache/apt/pkgcache.bin -mmin -5)"; then
 		sudo apt update
 	fi
 }
 
-if [ $# = 0 ]; then
-	show_help
-fi
+main() {
+	if test $# = 0; then
+		show_help
+	fi
 
-CMD="$1"
-shift 1
+	CMD="$1"
+	shift 1
 
-case "$CMD" in
-f*) dpkg -L "$@" ;;
-h*) show_help ;;
-add | i*)
-	check_pkgcache
-	sudo apt install "$@"
-	;;
-list-a*) apt list "$@" ;;
-list-i*) apt list --installed "$@" ;;
-rei*) apt install --reinstall "$@" ;;
-se*)
-	check_pkgcache
-	apt search "$@"
-	;;
-sh*) apt show "$@" ;;
-un* | rem* | rm | del*) sudo apt remove "$@" ;;
-up*)
-	sudo apt update
-	sudo apt upgrade
-	;;
-*)
-	echo "Unknown command: '$CMD' (run 'pkg help' for usage information)"
-	exit 1
-	;;
-esac
+	case "$CMD" in
+	f*) dpkg -L "$@" ;;
+	h*) show_help ;;
+	add | i*)
+		pkg_update
+		sudo apt install "$@"
+		;;
+	list-a*) apt list "$@" ;;
+	list-i*) apt list --installed "$@" ;;
+	rei*) apt install --reinstall "$@" ;;
+	se*)
+		pkg_update
+		apt search "$@"
+		;;
+	sh*) apt show "$@" ;;
+	un* | rem* | rm | del*) sudo apt remove "$@" ;;
+	up*)
+		pkg_update
+		sudo apt upgrade
+		;;
+	*)
+		echo "Unknown command: '$CMD' (run 'pkg help' for usage information)"
+		exit 1
+		;;
+	esac
+}
+
+main "$@"
